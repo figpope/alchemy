42b14ef5f6e7bdb1f621347129830a91b01d2f99
Brian Donghee Shin <shindonghee@gmail.com>
2012-09-15 00:58:48 -0400

    Move to coffee

--- public/app/js/controllers.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-# Controllers 
-MainCtrl = ->
-MyCtrl2 = ->
-"use strict"
-MyCtrl1.$inject = []
-MyCtrl2.$inject = []
---
+++ update.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+git pull
+coffee -bc -o public/app/js/ public/app/coffee 
