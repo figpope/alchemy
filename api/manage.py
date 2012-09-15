ec451ce691ee5dd22d0880b239ef56b9be942457
Andrew Pope <drewii2ii@gmail.com>
2012-09-15 05:20:04 -0400

    Fixed a typo

--- api/manage.py
+++ api/manage.py
@@ -3,7 +3,7 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask.ext.script import Manager, Server
-from alchemy import app
+from api import app
 
 manager = Manager(app)
 
